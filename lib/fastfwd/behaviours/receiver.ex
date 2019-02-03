8d62e75e0b4b6e51147382c19a5a0ad617256a36
Pete Birkinshaw <pete@binary-ape.org>
2019-02-03 08:46:36 +0000

    Docs and callback addition for fwd_map/0 added to the behaviours

--- lib/fastfwd/behaviours/receiver.ex
+++ lib/fastfwd/behaviours/receiver.ex
@@ -1,5 +1,13 @@
 defmodule Fastfwd.Behaviours.Receiver do
 
+  @moduledoc """
+  Default receiver Behaviour specification
+
+  This is the behaviour spec used by Fastfwd to quickly build receiver modules that will act as adapters or
+  plugins. Please see the Fastfwd.Receiver module for the default implementation of this behavior.
+
+  """
+
   @callback fwd_tags() :: [Atom.t]
 
 end
--- lib/fastfwd/behaviours/sender.ex
+++ lib/fastfwd/behaviours/sender.ex
@@ -1,6 +1,18 @@
 defmodule Fastfwd.Behaviours.Sender do
 
+  @moduledoc """
+  Default sender behaviour for Fastfwd.
+
+  This is the behaviour spec used by Fastfwd to quickly build sender modules that will act as
+  forwarders, delegetors, proxies or plugin interfaces.
+
+  Please see the Fastfwd.Sender module for the default implementation of this behavior.
+
+  """
+
+  @callback fwd(Atom.t, Atom.t, [Anything]) :: [Atom.t]
   @callback fwd_tags() :: [Atom.t]
   @callback fwd_modules() :: [Module.t]
+  @callback fwd_map() :: Map.t
 
 end
