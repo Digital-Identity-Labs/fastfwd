2363836529292d3e88e4f22dad59022c1f1ee383
Pete Birkinshaw <pete@binary-ape.org>
2019-02-20 09:24:34 +0000

    Improve caching in Sender module so that it's massively faster (by caching more)

--- lib/fastfwd/sender.ex
+++ lib/fastfwd/sender.ex
@@ -8,15 +8,16 @@ defmodule Fastfwd.Sender do
 
     namespace = Keyword.get(opts, :namespace, __CALLER__.module)
     behaviour = Keyword.get(opts, :behaviour, nil)
-    cache =     Keyword.get(opts, :cache, true)
-    fallback =  Keyword.get(opts, :fallback, nil)
-    autoload =  Keyword.get(opts, :autoload, true)
+    cache = Keyword.get(opts, :cache, true)
+    fallback = Keyword.get(opts, :fallback, nil)
+    autoload = Keyword.get(opts, :autoload, true)
     load_apps = Keyword.get(opts, :load_apps, :all)
 
     quote do
 
       @behaviour Fastfwd.Behaviours.Sender
       @fwd_modcache :"fastfwd/modcache/#{unquote(namespace)}/#{unquote(behaviour)}"
+      @fwd_mapcache :"fastfwd/mapcache/#{unquote(namespace)}/#{unquote(behaviour)}"
       @fwd_apploadcache :"fastfwd/apploadcache"
 
       @doc """
@@ -33,8 +34,7 @@ defmodule Fastfwd.Sender do
       @impl Fastfwd.Behaviours.Sender
       @spec fwd(atom, atom, list) :: Anything
       def fwd(tag, function_name, params) do
-        fwd_modules()
-        |> Fastfwd.select(tag)
+        fwd_map()[tag]
         |> apply(function_name, params)
       end
 
@@ -76,12 +76,17 @@ defmodule Fastfwd.Sender do
           cached_mods = FastGlobal.get(@fwd_modcache)
           if is_nil(cached_mods) do
             if unquote(autoload), do: {:ok, _} = fwd_cached_app_autoloader()
-            cached_mods = Fastfwd.modules(unquote(namespace), unquote(behaviour))
-            FastGlobal.put(@fwd_modcache, cached_mods)
+            mods = Fastfwd.modules(unquote(namespace), unquote(behaviour))
+            :ok = FastGlobal.put(@fwd_modcache, mods)
+            mods
+          else
+            cached_mods
           end
-          cached_mods
+        else
+          if unquote(autoload), do: {:ok, _} = fwd_cached_app_autoloader()
+          Fastfwd.modules(unquote(namespace), unquote(behaviour))
         end
-        Fastfwd.modules(unquote(namespace), unquote(behaviour))
+
       end
 
       @doc """
@@ -91,15 +96,27 @@ defmodule Fastfwd.Sender do
 
       ## Examples
 
-          Icecream.DoubleChocolate.fwd_tags()
+          Icecream.DoubleChocolate.fwd_map()
           [:chocolate, :double_chocolate]
 
       """
       @impl Fastfwd.Behaviours.Sender
       @spec fwd_map() :: Map.t()
       def fwd_map() do
-        fwd_modules()
-        |> Fastfwd.map()
+        if unquote(cache) do
+          cached_map = FastGlobal.get(@fwd_mapcache)
+          if is_nil(cached_map) do
+            map = fwd_modules()
+                  |> Fastfwd.map()
+            :ok = FastGlobal.put(@fwd_mapcache, map)
+            map
+          else
+            cached_map
+          end
+        else
+          fwd_modules()
+          |> Fastfwd.map()
+        end
       end
 
       ## Scan and load all modules in advance, and cache this to save a little time.
