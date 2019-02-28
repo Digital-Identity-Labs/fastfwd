9ca8543d493f13bbc26050171c91f1b351e9b2eb
Pete Birkinshaw <pete@binary-ape.org>
2019-02-28 16:23:45 +0000

    Docs for the sender and receiver modules

--- lib/fastfwd/receiver.ex
+++ lib/fastfwd/receiver.ex
@@ -3,6 +3,46 @@ defmodule Fastfwd.Receiver do
   @moduledoc """
   This module is a quick way to convert a module into a Fastfwd receiver and use it as an adapter or plugin.
 
+  ## Example
+
+  ```
+  defmodule Icecream.Strawberry do
+
+    use Fastfwd.Receiver, tags: [:strawberry]
+
+    def mix(tubs), do: "Mixing strawberry"
+
+  end
+
+  ```
+
+  ## Usage
+
+  Include this module's functions in your module by `use`ing it, and use the tags option to tag your module with a type.
+
+  ## Options
+
+  ### tags
+
+  A list of names/tags for this module, as atoms. These are the types that your module will handle.
+
+  If you are processing records in a database they may be stored be the `type` column, or as `driver` or `adapter`, but
+  Fastfwd uses `tag` as more neutral term.
+
+  #### Examples
+
+      use Fastfwd.Receiver, tags: [:wifi]
+
+      use Fastfwd.Receiver, tags: [:admin, :administrator]
+
+  ## Functions
+
+  Functions will be added to your module
+
+  * fwd_tags/0
+
+  See `Fastfwd.Behaviours.Receiver` for more information
+
   """
 
   defmacro __using__(opts) do
